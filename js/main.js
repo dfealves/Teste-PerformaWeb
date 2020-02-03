c705d1ab2169d904166940ce55aeed2ee17fbbff
dfealves <daniloferreira.alves@outlook.com>
2020-02-03 06:37:24 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,100 @@
+// map
+// filter
+// find
+// reduce
+// forEach
+
+// curry: uma função que retorna uma nova função
+
+const addOptionsIntoSelect = select => (value, label) => {
+  const option = document.createElement("option");
+  option.value = value;
+  option.innerHTML = label;
+
+  select.appendChild(option);
+};
+
+window.onload = () => {
+  const formData = new FormData();
+
+  const auth = {
+    user: "performaweb",
+    token: "pTspjI1jYxQngonfgbSPa"
+  };
+
+  const url = "https://www.api.performaweb.com.br/teste/";
+
+  const body = new FormData();
+
+  body.append("user", auth.user);
+  body.append("token", auth.token);
+  var request = new XMLHttpRequest();
+  request.open("POST", url);
+  request.send(body);
+
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      const responseJSON = JSON.parse(request.response);
+
+      const { estados } = responseJSON;
+
+      console.log(estados);
+
+      const selectEstados = document.querySelector("#estados");
+      selectEstados.disabled = false;
+
+      const selectCidades = document.querySelector("#cidades");
+      const imagemCidades = document.querySelector("#imagemCidade");
+
+      if (request.response && request.response.estados) {
+        request.response.estados.map(estado => {
+          console.log(estado);
+        });
+      }
+
+      selectEstados.addEventListener("change", function(ev) {
+        selectCidades.disabled = true;
+        const { value: sigla } = ev.target;
+
+        const { cidades, imagem } = estados.find(
+          estado => estado.sigla === sigla
+        );
+
+        if (cidades) {
+          const arraySelectCidades = Array.from(selectCidades.children);
+          const labelSelectCidades = arraySelectCidades.filter(
+            option => option.disabled
+          );
+
+          selectCidades.innerHTML = "";
+          selectCidades.appendChild(labelSelectCidades[0]);
+
+          cidades.forEach(cidade => {
+            addOptionsIntoSelect(selectCidades)(cidade, cidade);
+            imagemCidades.setAttribute("src", imagem);
+          });
+          selectCidades.disabled = false;
+        }
+      });
+
+      estados.forEach(estado => {
+        addOptionsIntoSelect(selectEstados)(estado.sigla, estado.nome);
+      });
+    }
+  };
+};
+
+function exibeButton() {
+  const divItem = document.querySelector(".item");
+  const btnComprar = document.createElement("button");
+
+  btnComprar.setAttribute("class", "btn-comprar");
+  btnComprar.innerHTML = "COMPRAR";
+
+  iconComprar = document.createElement("i");
+  iconComprar.setAttribute("class", "material-icons");
+  iconComprar.innerHTML = "add_shopping_cart";
+
+  divItem.appendChild(btnComprar);
+  btnComprar.appendChild(iconComprar);
+}
